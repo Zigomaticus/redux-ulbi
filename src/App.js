c32c210e0ec6e1a497cbbfc5871665722810572a
denis <permwed@gmail.com>
2022-03-29 08:52:22 +0500

    add redux

--- src/App.js
+++ src/App.js
@@ -1,17 +1,45 @@
 import { useDispatch, useSelector } from "react-redux";
+// Index
+// import { ADD_CASH, GET_CASH } from "./index";
 // Css
 import "./App.css";
 
 function App() {
   const dispatch = useDispatch();
   const cash = useSelector((state) => state.cash);
-  console.log(cash);
+
+  const addCash = (cash) => {
+    dispatch({
+      type: "ADD_CASH",
+      payload: cash,
+    });
+  };
+
+  const getCash = (cash) => {
+    dispatch({
+      type: "GET_CASH",
+      payload: cash,
+    });
+  };
 
   return (
     <div className="App">
-      <div style={{ display: "flex" }}>
-        <button>Add cash</button>
-        <button>Get cash</button>
+      <div className="cash">{cash}</div>
+      <div className="buttons">
+        <button
+          onClick={() => addCash(Number(prompt()))}
+          type="button"
+          className="btn btn-primary"
+        >
+          Add cash
+        </button>
+        <button
+          onClick={() => getCash(Number(prompt()))}
+          type="button"
+          className="btn btn-primary"
+        >
+          Get cash
+        </button>
       </div>
     </div>
   );
