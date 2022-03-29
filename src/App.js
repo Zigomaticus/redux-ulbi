14f0fa24453074c3375350f858fdc32586efbd66
denis <permwed@gmail.com>
2022-03-29 08:38:28 +0500

    start work

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import { useDispatch, useSelector } from "react-redux";
+// Css
+import "./App.css";
 
 function App() {
+  const dispatch = useDispatch();
+  const cash = useSelector((state) => state.cash);
+  console.log(cash);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div style={{ display: "flex" }}>
+        <button>Add cash</button>
+        <button>Get cash</button>
+      </div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,17 +1,40 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+// App
+import App from "./App";
+// Css
+import "./index.css";
+
+const defaultState = {
+  cash: 0,
+};
+
+const reducer = (state = defaultState, action) => {
+  switch (action.type) {
+    case "ADD_CASH":
+      return {
+        ...state,
+        cash: state.cash + action.payload,
+      };
+    case "GET_CASH":
+      return {
+        ...state,
+        cash: state.cash - action.payload,
+      };
+
+    default:
+      return state;
+  }
+};
+const store = createStore(reducer);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
